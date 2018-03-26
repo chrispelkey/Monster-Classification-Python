c1bead3cf3a2371cc2e21f6a4256d3c350587b6a
Chris <chris.pelkey@gmail.com>
2018-03-26 12:46:41 -0500

    Update Monsters Python.py

--- Monsters Python.py
+++ Monsters Python.py
@@ -96,3 +96,31 @@ ypred= logistic_model_1.predict(xtest)
 classification_report(ypred,ytest)
 
 knn.score(xtest, ytest)
+
+###Determine the best number of neighbors to have
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors = k)
+
+    # Fit the classifier to the training data
+    knn.fit(xtrain, ytrain)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(xtrain, ytrain)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(xtest, ytest)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
